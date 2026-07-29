71cad31d17f07471d179bd1a221483d3e07635a7
dreamsproxy <changfengjui@gmail.com>
2026-07-30 07:46:57 +0800

    Renamed core-0.py to net.py

---
+++ net.py
@@ -0,0 +1,10 @@
+import os
+from glob import glob
+
+def load_texts():
+    raw_texts = []
+    for p in glob('./datasets/*.txt'):
+        with open(p, mode='r', errors='replace', encoding='utf-8') as infile:
+            raw_texts.append(infile.readlines())
+    return raw_texts
+
