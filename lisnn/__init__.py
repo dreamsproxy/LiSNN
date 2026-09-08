3c5ec22c1417b6f951059aa3a4533706e075d488
Alan Chang <76526328+dreamsproxy@users.noreply.github.com>
2026-09-08 15:26:12 +0800

    Expose spatial substrate from lisnn package

--- lisnn/__init__.py
+++ lisnn/__init__.py
@@ -2,10 +2,14 @@
 
 from lisnn.network import SNN, create_nn, create_snn
 from lisnn.neurons import NeuronType, new_population
+from lisnn.spatial import SpatialConfig, SpatialSpec, SpatialVolume
 
 __all__ = [
     "SNN",
     "NeuronType",
+    "SpatialConfig",
+    "SpatialSpec",
+    "SpatialVolume",
     "create_nn",
     "create_snn",
     "new_population",
