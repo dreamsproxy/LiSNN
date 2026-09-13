08ce3f00b0453b5303e9e72998b2a81292125f23
Alan Chang <76526328+dreamsproxy@users.noreply.github.com>
2026-09-14 00:16:40 +0800

    Consolidate debugging and add the all-suite smoke runner

--- lisnn/debugging/__init__.py
+++ lisnn/debugging/__init__.py
@@ -2,8 +2,17 @@
 
 from lisnn.debugging.neuron_smoke import population_smoke_test
 from lisnn.debugging.synapse_smoke import synapse_smoke_test
+from lisnn.debugging.foundation_smoke import index_smoke_test, input_smoke_test, units_smoke_test
+from lisnn.debugging.network_smoke import network_smoke_test, spatial_smoke_test
+from lisnn.debugging.runner import run_all_smoke_tests
 
 __all__ = [
     "population_smoke_test",
     "synapse_smoke_test",
+    "index_smoke_test",
+    "input_smoke_test",
+    "units_smoke_test",
+    "network_smoke_test",
+    "spatial_smoke_test",
+    "run_all_smoke_tests",
 ]
